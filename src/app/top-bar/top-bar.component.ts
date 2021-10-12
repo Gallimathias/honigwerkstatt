da03522c1ada3fed95a302ee839fd59748ce3af1
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-10-12 22:24:11 +0200

    Edit top-bar component
    * Add instagram link and use the insta icon from fontawesome because its easier in using instead of using own

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
+import { faInstagram, IconDefinition } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-top-bar',
@@ -10,10 +12,16 @@ export class TopBarComponent implements OnInit {
 
   @Input() drawer?: MatDrawer;
   public showFiller: boolean = false;
+  public faInstagram: IconDefinition = faInstagram;
   
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  public openInNewTab(url: string): void{
+    window.open(url, "_blank");
   }
 
 }
