ecafaa558e7c9bdcb09e619da56670607abbb2aa
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-11-07 21:53:10 +0100

    Fix some typos
    * For more gramatical correctness fix all typos that we found

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -15,27 +15,28 @@ export class LandingPageComponent implements OnInit {
   public readonly supporter: PartnerLinkModel[];
 
   constructor() {
+
     this.landingTextBlockModels = [
       {
         title: 'Über uns',
         image: exampleUrl + '/assets/about.jpg',
         navLink: '/about',
         subtitle: 'Imkerei Brenner + Litmeier',
-        text: 'Die Imkerei wurde 2014 von mir gegründet. Nach dem interessanten Einführungstag in Hohenheim und einem spannenden Jungimkurs beim BV Nagold ging es los. Hier wurden uns viele Neuigkeiten und die Arbeiten im Bienenjahr in Praxis und Theorie gezeigt und die ersten beiden Völker wurden angeschafft.',
+        text: 'Die Imkerei wurde 2014 von mir gegründet. Nach dem interessanten Einführungstag in Hohenheim und einem spannenden Jungimker-Kurs beim BV Nagold ging es los. Hier wurden uns viele Neuigkeiten und die Arbeiten im Bienenjahr in Praxis und Theorie gezeigt und die ersten beiden Völker wurden angeschafft.',
       },
       {
-        title: 'die Biene',
+        title: 'Die Biene',
         image: exampleUrl + '/assets/honey.jpg',
         navLink: '/bee',
         subtitle: 'Botschafterin für mehr Artenvielfalt',
-        text: 'Die Biene befindet sich auf einem Höhenflug. Als sympathische Botschafterin für mehr Artenvielfalt ist die Popularität der fleißigen Pollensammlerin nicht völlig neu. Bekommt sie doch durch den Nachhaltigkeitstrend Aufwind und auch durch die aufrüttelnden Nachrichten über das Insektensterben.',
+        text: 'Die Biene befindet sich auf einem Höhenflug. Als sympathische Botschafterin für mehr Artenvielfalt ist die Popularität der fleißigen Pollen-Sammlerin nicht völlig neu. Bekommt sie doch durch den Nachhaltigkeitstrend Aufwind und auch durch die aufrüttelnden Nachrichten über das Insektensterben.',
       },
       {
         title: 'Naturnahe Firmengelände',
         image: exampleUrl + '/assets/peter_bees.jpg',
         navLink: '/project',
         subtitle: 'Gemeinsam für die Artenvielfalt',
-        text: 'Bei Fahrten durch verschiedene Gewerbegebiete ist uns aufgefallen, dass viele kurzgemähte Grünflächen die Firmengelände zieren, Büsche, Sträucher und Bäume säumen die Ränder. Für unsere heimischen Insekten und Vögel meistens nicht der optimale Lebensraum. Deshalb haben wir ein Konzept entwickelt, so dass auch mehr Lebensräume in Firmengelände für Insekten, Bienen, Wildbienen, Hummeln und Vögel entstehen können.',
+        text: 'Bei Fahrten durch verschiedene Gewerbegebiete ist uns aufgefallen, dass viele kurzgemähte Grünflächen die Firmengelände zieren, Büsche, Sträucher und Bäume säumen die Ränder. Für unsere heimischen Insekten und Vögel meistens nicht der optimale Lebensraum. Deshalb haben wir ein Konzept entwickelt, sodass auch mehr Lebensräume in Firmengelände für Insekten, Bienen, Wildbienen, Hummeln und Vögel entstehen können.',
       },
     ];
     this.ourPartners = [
