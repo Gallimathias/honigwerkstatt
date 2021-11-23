c873627b450ed4e9e268f298f4a7d020f9d38b7b
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-11-23 20:32:42 +0100

    Use a version
    * For debug porpose it makes sense to have a version number so i add this and log them out in the log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { from, fromEvent, fromEventPattern } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { WindowInformationService } from './window-information.service';
 
 @Component({
@@ -12,6 +13,6 @@ export class AppComponent {
   title = 'honigwerkstatt';
 
   constructor(public windowInformation: WindowInformationService) {
-    
+    console.log(`Current website version ${environment.version}`);
   }
 }
